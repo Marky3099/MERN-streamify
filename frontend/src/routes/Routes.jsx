32bf41645c62350dab6f0358ae4b85251eb2e2dd
Marky3099 <markreym0@gmail.com>
2025-09-11 09:47:55 +0800

    Frontend setup + pages + basic routes

---
+++ frontend/src/routes/Routes.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router";
+import HomePage from "../pages/HomePage.jsx";
+import RegisterPage from "../pages/RegisterPage.jsx";
+import LoginPage from "../pages/LoginPage.jsx";
+import NotificationPage from "../pages/NotificationPage.jsx";
+import CallPage from "../pages/CallPage.jsx";
+import ChatPage from "../pages/ChatPage.jsx";
+import OnboardingPage from "../pages/OnboardingPage.jsx";
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/notifications" element={<NotificationPage />} />
+    <Route path="/call" element={<CallPage />} />
+    <Route path="/chat" element={<ChatPage />} />
+    <Route path="/onbaording" element={<OnboardingPage />} />
+  </Routes>
+);
+
+export default AppRoutes;
